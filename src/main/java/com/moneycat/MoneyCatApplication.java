fdc9c339c0aed9b625b0750ef423b466c7370c51
nyximos <nyximos@gmail.com>
2024-09-17 16:19:41 +0900

    chore: first commit (#1)

---
+++ src/main/java/com/moneycat/MoneyCatApplication.java
@@ -0,0 +1,17 @@
+package com.moneycat;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyCatApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(MoneyCatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moneycat/MoneyCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
